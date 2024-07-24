b531b92244e5f0fef11338ac488e8c0dfac9b03a
aansd <septiyan63@gmail.com>
2024-07-24 23:47:25 +0700

    post reactjs x mongoDB

--- server/app/product-v2/validator.js
+++ server/app/product-v2/validator.js
@@ -4,5 +4,4 @@ exports.createValidator = [
     check('name', 'nama wajib di isi').not().isEmpty(),
     check('price', 'harga wajib di isi').not().isEmpty(),
     check('stock', 'stock wajib di isi').not().isEmpty(),
-
 ]
